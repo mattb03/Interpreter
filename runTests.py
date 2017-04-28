866c6d2a7ef4835585726ddde31bee46b379e24e
mattb03 <mattb03@yahoo.com>
2017-04-27 21:07:35 -0500

    fixed nested counting loop error test cases

--- runTests.py
+++ runTests.py
@@ -12,10 +12,7 @@ for i in range (0, len(testFiles)):
     if (index == i):
         pids.append(index)
         pids[index] = os.fork()
-        #call(["java", "-classpath", "bin", "havabol.HavaBol", "for_loop.txt"])
         if (pids[index] == 0):
-            #print ("i am process " + str(index))
-            #call(["ls", "-l"])
             call(["java", "-classpath", "bin", "havabol.HavaBol", sys.argv[1] + testFiles[index]])
             sys.exit()
 
